3115b27381153f1dd2aa29c15995c5a91a44b0ac
Edznux <edznux@gmail.com>
2019-02-11 02:19:36 +0100

    Add "at" command flag for add

--- cmd/add.go
+++ cmd/add.go
@@ -7,7 +7,12 @@ import (
 
 	"github.com/edznux/metastats/config"
 	"github.com/edznux/metastats/events"
+	"github.com/olebedev/when"
 	"github.com/spf13/cobra"
+)
+
+var (
+	at string
 )
 
 // addCmd represents the add command
@@ -17,14 +22,31 @@ var addCmd = &cobra.Command{
 	Example: `metastats add steps 1337
 	will add 1337 and the current timestamp in the file steps.csv`,
 	Run: func(cmd *cobra.Command, args []string) {
+		var date int64
+
 		cfg := config.LoadConfig()
+		if len(args) == 0 {
+			fmt.Println("Please provide a dataset name")
+			return
+		}
+
 		fileName := cfg.DataPath + args[0] + ".csv"
 
-		date := time.Now().Unix()
+		if at == "" {
+			date = time.Now().Unix()
+		} else {
+			res, err := when.EN.Parse(at, time.Now())
+			if err != nil {
+				fmt.Printf("Could not parse the date : %s\n", err)
+				return
+			}
+			date = res.Time.Unix()
+		}
 
 		data := []string{
 			strconv.FormatInt(date, 10),
 		}
+
 		data = append(data, args[1:]...)
 
 		if cfg.Verbose {
@@ -34,10 +56,12 @@ var addCmd = &cobra.Command{
 		err := events.SaveToFile(fileName, data)
 		if err != nil {
 			fmt.Printf("Could not save this new event : %s\n", err)
+			return
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(addCmd)
+	addCmd.PersistentFlags().StringVar(&at, "at", "", "Use a custom time. Human readable format (today 2pm for example)")
 }
