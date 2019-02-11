d801924fba42d65ea2853a7685d5d9ee63b4f6f2
Edznux <edznux@gmail.com>
2019-02-11 04:04:54 +0100

    Better install.sh and add config.example.toml

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo systemctl enable metastats
 
 # copy config file
 sudo mkdir -p /etc/metastats/
-sudo cp config.toml /etc/metastats/
+sudo cp config.example.toml /etc/metastats/config.toml
 
 # copy binary
 sudo cp metastats /usr/local/bin/metastats
@@ -19,7 +19,12 @@ sudo systemctl start metastats
 sudo systemctl status metastats
 
 # Install bash completion
-echo "source <(metastats completion)" >> ~/.bashrc
+METASTATS_BASHRC="source <(metastats completion)"
+if grep --quiet $METASTATS_BASHRC ~/.bashrc; then
+    echo "Already in your ~/.bashrc"
+else
+    echo $METASTATS_BASHRC >> ~/.bashrc
+fi
 
 # TODO : add rclone saving.
 # crontab -e
